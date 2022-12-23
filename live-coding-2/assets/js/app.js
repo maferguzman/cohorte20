e5d8d7e4b70ec0decba91b88b4d0c2dfa53cdd68
mafer <maria.guzmancc@gmail.com>
2022-12-23 16:23:19 -0600

    live coding 2

---
+++ live-coding-2/assets/js/app.js
@@ -0,0 +1,31 @@
+//no hay anotaciones ni comentarios (no se entiende para que es el código)
+//variables tienen nombre no descriptivo, con signo de $
+//llamadas al DOM deben especificar tipo de selector
+//nombre de selectores y sus tipos no coinciden con los del html
+
+
+//función tiene un await 
+//función(es) no se entienden bien
+
+const baseEndpoint = 'https://api.github.com';
+const usersEndpoint = `${baseEndpoint}/users`;
+const $n = document.querySelector('name');
+const $b = document.querySelector('#blog');
+const $l = document.querySelector('.location');
+
+function displayUser(username) {
+  $n.textContent = 'cargando...';
+  const response = await fetch(`${usersEndpoint}/${username}`);
+  console.log(data);
+  $n.textContent = '${data.name}';
+  $b.textContent = '${data.blog}';
+  $l.textContent = '${data.location}';
+}
+
+function handleError(err) {
+  console.log('OH NO!');
+  console.log(err);
+  n.textContent = `Algo salió mal: ${err}`
+}
+
+displayUser('stolinski').catch(handleError);
